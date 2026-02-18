7e8f8a3e0be9f3a9d06bc49d84cf5f6f509d10e7
Harsh Vardhan Bhanot <152215921+hvbhanot@users.noreply.github.com>
2026-02-17 21:18:42 -0600

    Add solution for normalize-3d

---
+++ normalize-3d/normalize-3d.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def normalize_3d(v):
+    """
+    Normalize 3D vector(s) to unit length.
+    """
+    v = np.array(v)
+    dims = v.ndim
+    if dims == 1:
+      norm = np.linalg.norm(v, keepdims=True)
+      submission = v / norm
+      
+    else:  
+      norm = np.linalg.norm(v, axis=1)
+      norm[norm == 0] = 1  # Fixing the divide issue for zero vectors
+      submission = v / norm[:, np.newaxis] 
+
+    return submission
