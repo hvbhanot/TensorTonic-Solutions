a08bc35de11807ce59fc1d39e738f9b88e0dbe46
Harsh Vardhan Bhanot <152215921+hvbhanot@users.noreply.github.com>
2026-03-07 00:09:38 -0600

    Update solution for gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -3,10 +3,12 @@ def gradient_descent_quadratic(a, b, c, x0, lr, steps):
     Return final x after 'steps' iterations.
     """
 
-    min = x0
+    global_min = x0
 
-    for i in range(steps):
-      min = min - lr*((2*a*min) + (b))
+    
+    for _ in range(steps):
+     deriv =  2*a*global_min + b
+     global_min = global_min - lr*deriv
 
-
-    return min
+    return global_min
+  
