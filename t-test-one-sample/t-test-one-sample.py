e38f48ccc20c3fdc04a716f0b7b6cf179ffa73ae
Harsh Vardhan Bhanot <152215921+hvbhanot@users.noreply.github.com>
2026-03-25 22:33:07 -0500

    Update solution for t-test-one-sample

--- t-test-one-sample/t-test-one-sample.py
+++ t-test-one-sample/t-test-one-sample.py
@@ -5,7 +5,7 @@ def t_test_one_sample(x, mu0):
     Compute one-sample t-statistic.
     """
     mean = np.mean(x)
-    s = np.std(x, ddof=1)
+    s = np.std(x, ddof=1) # Bessal Correction using ddof
 
     t_stat = (mean - mu0) / (s/ np.sqrt(len(x)))
     return t_stat
