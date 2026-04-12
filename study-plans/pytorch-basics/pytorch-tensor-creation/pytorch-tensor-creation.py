9bb3b4a39b30e2f329607b51d95db795a80209da
Harsh Vardhan Bhanot <152215921+hvbhanot@users.noreply.github.com>
2026-04-12 16:25:38 -0500

    Add solution for pytorch-tensor-creation

---
+++ study-plans/pytorch-basics/pytorch-tensor-creation/pytorch-tensor-creation.py
@@ -0,0 +1,15 @@
+import torch
+
+def create_tensor(method, shape, value=0.0):
+    """
+    Returns: list
+    """
+
+    if method == 'zeros':
+        return (torch.zeros(*shape).tolist())
+        
+    elif method == 'ones':
+        return (torch.ones(*shape).tolist())
+
+    else:
+        return (torch.full(shape, value).tolist()) # Full accepts the 
