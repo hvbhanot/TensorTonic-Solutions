8a6c31b6ef5a94c12461c7c7ceb0790978aae63c
Harsh Vardhan Bhanot <152215921+hvbhanot@users.noreply.github.com>
2026-04-12 23:28:59 -0500

    Add solution for pytorch-tensor-arithmetic

---
+++ study-plans/pytorch-basics/pytorch-tensor-arithmetic/pytorch-tensor-arithmetic.py
@@ -0,0 +1,23 @@
+import torch
+
+def tensor_op(x, y, op):
+    """
+    Returns: list (result tensor converted via .tolist())
+    """
+    x = torch.tensor(x)
+    y = torch.tensor(y)
+
+    if op == 'add':
+        return (torch.add(x,y)).tolist()
+
+    elif op == 'multiply':
+        return (torch.mul(x,y)).tolist()
+
+    elif op == 'matmul':
+        return (torch.matmul(x,y)).tolist()
+
+    elif op == 'power':
+        return (torch.pow(x,y)).tolist()
+
+    else:
+        return torch.maximum(x, y).tolist()
