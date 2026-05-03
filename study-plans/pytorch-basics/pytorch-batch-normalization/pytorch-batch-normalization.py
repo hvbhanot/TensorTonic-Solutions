459b0cf5133e2862811eca2c56798636617f5078
Harsh Vardhan Bhanot <152215921+hvbhanot@users.noreply.github.com>
2026-05-02 21:50:51 -0500

    Add solution for pytorch-batch-normalization

---
+++ study-plans/pytorch-basics/pytorch-batch-normalization/pytorch-batch-normalization.py
@@ -0,0 +1,14 @@
+import torch
+
+def batch_norm(X, gamma, beta, eps=1e-5):
+    """
+    Returns: tensor of shape (N, D), the batch-normalized output
+    """
+    mean = torch.mean(X, dim=0)
+    var = torch.var(X, dim=0, unbiased=False) # 
+
+    Xhat = (X-mean)/torch.sqrt(var + eps)
+
+    Y = gamma*Xhat + beta
+
+    return Y
